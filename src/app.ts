a0fe5398e344169f9712e58f9da68121e61b0a9c
Ilona Mereuta <edoilo.3521@gmail.com>
2023-01-09 13:29:53 +0100

    add get /planets route

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,11 @@ import "express-async-errors";
 const app = express();
 
 
-app.get("/", (request, response) => {
-    response.send("This is the space API!!!!!!!!");
+app.get("/planets", (request, response) => {
+    response.json([
+        { name: "Mercury" },
+        { name: "Venus" }
+    ]); //it will be formated as a json
 });
 
 export default app;
