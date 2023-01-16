f2ed57de12c4f2d005b06a1520192f81ab03ebf7
Ilona Mereuta <edoilo.3521@gmail.com>
2023-01-16 14:27:42 +0100

    add mock for prisma client

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,17 @@
 import express from "express";
 import "express-async-errors";
+import prisma from "./lib/prisma/client";
 
 const app = express();
 
 
-app.get("/planets", (request, response) => {
+app.get("/planets", async (request, response) => {
 
-    const planets = [
-        { name: "Mercury" },
-        { name: "Venus" }
-    ]; //it will be formated as a json
+   const planets = await prisma.planet.findMany();
+
+   response.json(planets);
 
 
-    //console.log( {planets} )
-    // console.table(planets)
-
-    // response.json(planets)
 });
 
 export default app;
--- src/server.test.ts
+++ src/server.test.ts
@@ -1,16 +1,41 @@
 import supertest from "supertest";
-import app from './app';
+
+import { prismaMock } from "./lib/prisma/client.mock";
+
+import app from "./app";
 
 const request = supertest(app);
 
-test("GET /planets", async() => {
-    const response = await request
-    .get("/planets")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+test("GET /planets", async () => {
+    const planets = [
+        //set what we're expecting to come back
+        {
+            id: 1,
+            name: "Mercury",
+            description: "planet in our system",
+            diameter: 223455,
+            moons: 34,
+            createdAt: "2023-01-16T12:27:38.321Z",
+            updatedAt: "2023-01-16T12:28:47.863Z",
+        },
+        {
+            id: 2,
+            name: "Venus",
+            description: null,
+            diameter: 3344,
+            moons: 33,
+            createdAt: "2023-01-16T12:29:02.141Z",
+            updatedAt: "2023-01-16T12:28:49.614Z",
+        },
+    ];
 
-    expect(response.body).toEqual([
-        { name: "Mercury" },
-        { name: "Venus" }
-    ]);
-})
+    //@ts-ignore
+    prismaMock.planet.findMany.mockResolvedValue(planets); //we're passing this planets array as the mock value
+
+    const response = await request
+        .get("/planets")
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+
+    expect(response.body).toEqual(planets); //change what we're expecting here
+});
