48adfc7571a75c153b412aa77e7137cea6d2a46f
Ilona Mereuta <edoilo.3521@gmail.com>
2023-01-19 12:49:33 +0100

    get all planets from db done

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,63 @@
 import express from "express";
 import "express-async-errors";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 
-
-app.get("/planets", (request, response) => {
-
-    const planets = [
-        { name: "Mercury" },
-        { name: "Venus" }
-    ]; //it will be formated as a json
+//new instance of prisma client
+const prisma = new PrismaClient();
 
 
-    //console.log( {planets} )
-    // console.table(planets)
+app.get("/planets", async (request, response) => {
+    const planets = await prisma.planet.findMany();
 
-    // response.json(planets)
+    response.json(planets);
+
 });
 
 export default app;
+
+
+
+
+/* Here is the respose from POSTMAN with all the planets from the databse:
+
+[
+    {
+        "id": 1,
+        "name": "Mercury",
+        "description": "planet in our system",
+        "diameter": 223455,
+        "moons": 34,
+        "createdAt": "2023-01-16T12:27:38.321Z",
+        "updatedAt": "2023-01-16T12:28:47.863Z"
+    },
+    {
+        "id": 2,
+        "name": "Venus",
+        "description": null,
+        "diameter": 3344,
+        "moons": 33,
+        "createdAt": "2023-01-16T12:29:02.141Z",
+        "updatedAt": "2023-01-16T12:28:49.614Z"
+    },
+    {
+        "id": 3,
+        "name": "Mercury",
+        "description": null,
+        "diameter": 223455,
+        "moons": 34,
+        "createdAt": "2023-01-16T22:16:17.269Z",
+        "updatedAt": "2023-01-16T22:16:17.269Z"
+    }
+]
+
+
+
+
+
+
+
+
+
+*/
