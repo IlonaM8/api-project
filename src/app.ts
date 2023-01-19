750b7194eeeb775195a1ceb9cb6ddec01e412c52
Ilona Mereuta <edoilo.3521@gmail.com>
2023-01-19 14:04:27 +0100

    add post planet route

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,10 @@ const app = express();
 //new instance of prisma client
 const prisma = new PrismaClient();
 
+//midleware
+app.use(express.json());
 
+//get all resources from db
 app.get("/planets", async (request, response) => {
     const planets = await prisma.planet.findMany();
 
@@ -15,49 +18,16 @@ app.get("/planets", async (request, response) => {
 
 });
 
+//creating a new resource with POST method
+app.post("/planets", async (request, response) => {
+    const planet = request.body;
+
+    response.status(201).json(planet);
+
+});
+
+
+
 export default app;
 
 
-
-
-/* Here is the respose from POSTMAN with all the planets from the databse:
-
-[
-    {
-        "id": 1,
-        "name": "Mercury",
-        "description": "planet in our system",
-        "diameter": 223455,
-        "moons": 34,
-        "createdAt": "2023-01-16T12:27:38.321Z",
-        "updatedAt": "2023-01-16T12:28:47.863Z"
-    },
-    {
-        "id": 2,
-        "name": "Venus",
-        "description": null,
-        "diameter": 3344,
-        "moons": 33,
-        "createdAt": "2023-01-16T12:29:02.141Z",
-        "updatedAt": "2023-01-16T12:28:49.614Z"
-    },
-    {
-        "id": 3,
-        "name": "Mercury",
-        "description": null,
-        "diameter": 223455,
-        "moons": 34,
-        "createdAt": "2023-01-16T22:16:17.269Z",
-        "updatedAt": "2023-01-16T22:16:17.269Z"
-    }
-]
-
-
-
-
-
-
-
-
-
-*/
--- src/server.test.ts
+++ src/server.test.ts
@@ -14,3 +14,24 @@ test("GET /planets", async() => {
         { name: "Venus" }
     ]);
 })
+
+
+
+//making a test POST for 'creating something new'
+test("POST /planets", async() => {
+     const planet = {
+        name: "TOI 700 b",
+        diameter: 7.581,
+        moons: 1
+    };
+
+
+    const response = await request
+    .post("/planets")
+    .send(planet)
+    .expect(201)  //response 201 - something new is being created
+    .expect("Content-Type", /application\/json/);
+
+    expect(response.body).toEqual(planet);
+});
+
